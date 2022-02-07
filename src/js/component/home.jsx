b9d0eb9f06741bc8917d50b49569aa0bf36450ec
Dustm00n <gerardoserranosoto@gmail.com>
2022-02-07 17:50:05 +0000

    casi completo me falta el botoncito

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const TrafficLight = () => {
+	const [color, setColor] = useState("");
+	function Pincha() {
+		setColor(setColor);
+	}
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="base-light"></div>
+			<div className="traffic-light">
+				<div
+					className={"light red" + (color === "red" ? " glow1" : "")}
+					onClick={() => setColor("red")}></div>
+				<div
+					className={
+						"light yellow" + (color === "yellow" ? " glow2" : "")
+					}
+					onClick={() => setColor("yellow")}></div>
+				<div
+					className={
+						"light green" + (color === "green" ? " glow3" : "")
+					}
+					onClick={() => setColor("green")}></div>
+			</div>
+			<button onClick={() => Pincha()}>PINCHA!!!</button>;
+		</>
 	);
 };
-
-export default Home;
+export default TrafficLight;
